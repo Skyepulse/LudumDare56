b01e0ea615755277a74e419d63547b2f9915b4a6
Gaspar <gaspar.ligot.14@gmail.com>
2024-10-05 23:41:20 +0200

    Fixed (?) gaspar bug

--- PrefabScenes/general.gd
+++ PrefabScenes/general.gd
@@ -4,20 +4,19 @@ extends Node2D
 # @onready var size_camera = get_viewport_rect().size() 
 
 #var top_left_position = (position_camera - (0.5*size_camera))
+var voiture: PackedScene = preload("res://PrefabScenes/voiture.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	while (1):
 		var rand = randf()
 		if rand <= 0.2:
 			_on_timer_timeout()
 		
 
 func _on_timer_timeout() -> void:
-	var voiture: PackedScene = preload("res://PrefabScenes/voiture.tscn")
 	var voiture_instance = voiture.instance()
 	add_child(voiture_instance)
 	voiture_instance.position = Vector2(100, 100)
