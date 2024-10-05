33a4f7c545feed27995dab95721ddc6771c89f93
Gaspar <gaspar.ligot.14@gmail.com>
2024-10-05 23:05:50 +0200

    modif

--- PrefabScenes/voiture.gd
+++ PrefabScenes/voiture.gd
@@ -1,16 +1,13 @@
-class_name Voiture
-
 extends CharacterBody2D
 
-@export var speed : int
-
+var speed = 100
+var direction = Vector2(0, -1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	velocity += speed.direction
+	move_and_slide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	velocity = speed*Vector2(1,0)
-	move_and_slide()
-	
+	pass
