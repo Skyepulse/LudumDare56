738db9b594f966c5370dcec560aaa33e81a9fc2a
Gaspar <gaspar.ligot.14@gmail.com>
2024-10-06 01:34:16 +0200

    voiture_defile

--- PrefabScenes/general.gd
+++ PrefabScenes/general.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-# @onready var position_camera = get_screen_center_position()
-# @onready var size_camera = get_viewport_rect().size() 
-
-#var top_left_position = (position_camera - (0.5*size_camera))
 var voiture: PackedScene = preload("res://PrefabScenes/Voiture.tscn")
 
 # Called when the node enters the scene tree for the first time.
