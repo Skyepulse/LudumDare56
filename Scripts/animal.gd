26ba1530b0c14aade168d72d697f2c695f078a2f
Ipieh <pierina1000a@gmail.com>
2024-10-05 16:43:12 +0200

    machine etat animal

--- Scripts/animal.gd
+++ Scripts/animal.gd
@@ -3,13 +3,13 @@ extends CharacterBody2D
 class_name Animal
 
 enum AnimalType {
-    SLUG = 0,
-    BETTER_SLUG= 1,
-    TOAD =2,
-    SNAKE = 3,
-    CHICKEN = 4,
-    FOX = 5,
-    ALIEN = 6
+	SLUG = 0,
+	BETTER_SLUG= 1,
+	TOAD =2,
+	SNAKE = 3,
+	CHICKEN = 4,
+	FOX = 5,
+	ALIEN = 6
 }
 
 @export var speed : int
@@ -18,3 +18,10 @@ enum AnimalType {
 @export var reproduction_rate : int
 @export var type : AnimalType
 @export var ui_sprite: Texture2D
+
+enum AnimalState {
+	STACKED = 0, # animal dans le stack
+	HELD = 1, # animal pret a etre lance
+	CROSSING = 2, #traverse la route
+	FREE = 3 #animal sauvage mange les autres
+}
