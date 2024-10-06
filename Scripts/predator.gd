dd6dbf33ca38188314195a257bb121cefedc5ba0
Telo <telo.philippe@telecom-paris.fr>
2024-10-06 13:46:11 +0200

    Improved predators

--- Scripts/predator.gd
+++ Scripts/predator.gd
@@ -2,6 +2,15 @@ extends CharacterBody2D
 
 class_name Predator
 
+enum PredatorState {
+	ATTACKING = 0,
+	EATING = 1,
+	FLEEING = 2,
+	HELD = 3
+}
+
+var state: PredatorState = PredatorState.ATTACKING
+
 var target_stack: Vivier
 @export var speed = 50 
 
@@ -11,5 +20,14 @@ func _ready():
 
 func _process(delta):
 	# Predator velocity is towards the target
-	velocity = (target_stack.position - position).normalized() * speed
-	move_and_slide()
+	if state == PredatorState.ATTACKING:
+		velocity = (target_stack.position - position).normalized() * speed
+		move_and_slide()
+	elif state == PredatorState.HELD:
+		global_position = get_global_mouse_position()
+
+func on_press():
+	state = PredatorState.HELD
+			
+func on_release():
+	state = PredatorState.ATTACKING
