42f6b562e293245452dd32e51b15abd3cb0c2598
Telo <telo.philippe@telecom-paris.fr>
2024-10-07 00:52:08 +0200

    set d&d to pick an unique animal

--- Scripts/drag_and_drop.gd
+++ Scripts/drag_and_drop.gd
@@ -2,9 +2,13 @@ extends Area2D
 
 @onready var animal = $".."
 
+static var picked: bool = false
+
 func _input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
-		if event.is_pressed():
+		if not picked and event.is_pressed():
+			picked = true
 			animal.on_press()
 		elif event.is_released():
+			picked = false
 			animal.on_release()
