5e684b6553e2b126ce806bfb48671b4a7a8e7492
Telo <telo.philippe@telecom-paris.fr>
2024-10-06 14:37:33 +0200

    Finished predators

--- Scripts/predator.gd
+++ Scripts/predator.gd
@@ -10,9 +10,14 @@ enum PredatorState {
 }
 
 var state: PredatorState = PredatorState.ATTACKING
+var is_in_stack: bool = false
 
 var target_stack: Vivier
 @export var speed = 50 
+@export var kind: Animal.AnimalType
+
+@onready var timer = $Timer
+
 
 func _ready():
 	# Set the predator's position a random position in the world
@@ -23,6 +28,12 @@ func _process(delta):
 	if state == PredatorState.ATTACKING:
 		velocity = (target_stack.position - position).normalized() * speed
 		move_and_slide()
+		if is_in_stack:
+			state = PredatorState.EATING
+			timer.start()
+	if state == PredatorState.FLEEING:
+		velocity = -(target_stack.position - position).normalized() * speed * 2
+		move_and_slide()
 	elif state == PredatorState.HELD:
 		global_position = get_global_mouse_position()
 
@@ -30,4 +41,16 @@ func on_press():
 	state = PredatorState.HELD
 			
 func on_release():
-	state = PredatorState.ATTACKING
+	if is_in_stack:
+		if Vivier.instance:
+			Vivier.instance.instantiate_animal(kind)
+			queue_free()
+		else:
+			push_error("No vivier instance in the scene!")
+	else:
+		state = PredatorState.FLEEING
+
+
+func _on_timer_timeout():
+	state = PredatorState.FLEEING
+	Vivier.instance.remove_random_animal(kind)
