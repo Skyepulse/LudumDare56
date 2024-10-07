11cc1a2e2a561970673308a6bc2a4310442b2b5e
Telo <telo.philippe@telecom-paris.fr>
2024-10-07 11:17:45 +0200

    Fix lvl1

--- Scripts/Levels/level_1.gd
+++ Scripts/Levels/level_1.gd
@@ -1,7 +1,7 @@
 extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	LevelNumber.level_number = 2; # Replace with function body.
+	LevelNumber.level_number = 1; # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
