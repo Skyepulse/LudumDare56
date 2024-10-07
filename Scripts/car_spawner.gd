614c58cb8194856a4812678297baad2de71ed3bb
Gaspar <gaspar.ligot.14@gmail.com>
2024-10-07 21:21:12 +0200

    route_3_voie

--- Scripts/car_spawner.gd
+++ Scripts/car_spawner.gd
@@ -10,12 +10,19 @@ var camion: PackedScene = preload("res://PrefabScenes/Voitures/Camion.tscn")
 
 @export var delay = 1 #Time between car
 @export var car_speed = 200
-@export var proba_droite = 0.1
 @export var proba_gauche = 0.1
+@export var proba_droite = 0.1
+@export var proba_droite_2 = 0.1
+
+@export var nbr_route = 3
+
 var time_passed = 0.0
 
 var top_left_position
 var top_right_position
+var top_right_position_2
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,27 +30,41 @@ func _ready() -> void:
 	#Camera
 	var position_camera = camera.get_screen_center_position()
 	var size_camera = get_viewport_rect().size
-	top_left_position = (position_camera - (0.5*size_camera) + Vector2(0,102))
-	var top_route = (position_camera - (0.5*size_camera))
 
+	top_left_position = (position_camera - (0.5*size_camera) + Vector2(0,102))
 	top_right_position = (position_camera + (0.5*size_camera)*Vector2(1,-1) + Vector2(0,250))
-	route.position = top_route + Vector2(route.texture.get_width() - 100,route.texture.get_height())/2
+	top_right_position_2 = (position_camera + (0.5*size_camera)*Vector2(1,-1) + Vector2(0,398))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	time_passed += delta  # Incrémente le temps écoulé
-	var rand_gauche = randf()
-	var rand_droite = randf()
 
-	if time_passed >= delay:
-		if rand_gauche <= proba_droite: #probability that there is a car on delay
-			voiture_move(top_left_position, 2, true)
-		if rand_droite <= proba_gauche: #probability that there is a car on delay
-			voiture_move(top_right_position, 1, false)
-		time_passed = 0.0
+	if nbr_route == 2:
+		var rand_gauche = randf()
+		var rand_droite = randf()
+		if time_passed >= delay:
+			if rand_gauche <= proba_gauche: #probability that there is a car on delay
+				voiture_move(top_left_position, true)
+			if rand_droite <= proba_droite: #probability that there is a car on delay
+				voiture_move(top_right_position, false)
+			time_passed = 0.0
+
+	elif nbr_route == 3:
+		var rand_gauche = randf()
+		var rand_droite_1 = randf()
+		var rand_droite_2 = randf()
+		if time_passed >= delay:
+			if rand_gauche <= proba_gauche: #probability that there is a car on delay
+				voiture_move(top_left_position, true)
+			if rand_droite_1 <= proba_droite: #probability that there is a car on delay
+				voiture_move(top_right_position, false)
+			if rand_droite_2 <= proba_droite: #probability that there is a car on delay
+				voiture_move(top_right_position_2, false)
+			time_passed = 0.0
+
 			
 	   
-func voiture_move(camera_position, direc: int,a: bool ) -> void:
+func voiture_move(camera_position,a: bool ) -> void:
 	var rand = randf()
 	var voiture_instance
 	if rand <=0.4:
@@ -56,6 +77,6 @@ func voiture_move(camera_position, direc: int,a: bool ) -> void:
 	voiture_instance.get_node("AnimatedSprite2D").flip_h = a
 	voiture_instance.speed = car_speed
 	
-	voiture_instance.direc = direc
+	voiture_instance.direc = a
 	add_child(voiture_instance)
 	voiture_instance.position = camera_position
--- Scripts/voiture.gd
+++ Scripts/voiture.gd
@@ -4,17 +4,18 @@ var speed = 1000
 var direction_droite = Vector2(-1, 0)
 var direction_gauche = Vector2(1, 0)
 
-var direc: int
+var direc: bool
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if(direc==1):
-		velocity = speed*direction_droite
-	else:
+	if(direc==true):
 		velocity = speed*direction_gauche
+	else:
+		velocity = speed*direction_droite
+
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		var animal = collision.get_collider()
