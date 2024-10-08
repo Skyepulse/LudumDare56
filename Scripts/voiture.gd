2d5dc1a5e0b62d2e17d897e44d5745b71df89377
Telo <telo.philippe@telecom-paris.fr>
2024-10-08 10:26:29 +0200

    Added triple roads

--- Scripts/voiture.gd
+++ Scripts/voiture.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 var speed = 1000
-var direction_droite = Vector2(-1, 0)
-var direction_gauche = Vector2(1, 0)
+var direction_droite = Vector2(1, 0)
+var direction_gauche = Vector2(-1, 0)
 
 var direc: int
 # Called when the node enters the scene tree for the first time.
